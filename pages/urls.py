85aaa9042c81bcccc9e1b1caa518f66750c95cc7
Flavio <fbve68@gmail.com>
2020-10-03 15:15:43 -0300

    New updates for Heroku deployment

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 
 
-urlpatterns = [
+urlpatterns = [ 
     path('about/', AboutPageView.as_view(), name='about'), 
     path('', HomePageView.as_view(), name='home'),
 ]
--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0t=5*hsxs$!$gg-2tw(--penm6#w+$&nsgm%8%0lj8r*s!8ng2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pages_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
